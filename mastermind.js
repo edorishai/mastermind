e4d50019f23dd6c198a0bb26033aa031209107f1
edorishai <adrienlasson2006@gmail.com>
2025-11-04 15:59:39 +0100

    test-nb

--- mastermind.js
+++ mastermind.js
@@ -1,13 +1,25 @@
-    document.getElementById("monForm").addEventListener("submit", function(e) {
-      e.preventDefault(); 
 
-      let couleur = document.getElementById("couleur").value; 
-      let zone = document.getElementById("affichage");
 
-      zone.innerHTML += couleur + "<br>";
+    function genererCombinaison() {
+      let combinaison = [];
+      for (let i = 0; i < 4; i++) {
+        let chiffre = Math.floor(Math.random() * 6) + 1; 
+        combinaison.push(chiffre);
+      }
+      return combinaison;
+    }
 
-      document.getElementById("couleur").value = "";
+    let combinaisonSecrete = genererCombinaison();
+    console.log("Combinaison secrète :", combinaisonSecrete.join(""));
+
+    document.getElementById("valider").addEventListener("click", function() {
+      let couleur = document.getElementById("couleur").value;
+      let zone = document.getElementById("zone");
+
+      if (couleur === combinaisonSecrete.join("")) {
+        zone.innerHTML += " Bonne combinaison : " + couleur + "<br>";
+      } else {
+        zone.innerHTML += " Mauvaise combinaison : " + couleur + "<br>" + combinaisonSecrete ;
+      }
     });
 
-
-    
